8e5d67b28d2a647731f97ca9b925783cbbcf0132
Rizky Agung Prayogi <rizkyden06@gmail.com>
2022-06-24 09:25:00 +0700

    update

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,7 @@
 {{-- @dd($post) --}}
 @extends('layout.main')
 @section('content')
-    <article>
+    <article class="mt-3 mb-3">
         <h1>{{ $post->title }}</h1>
         <h5>{{ $post->author }}</h5>
         {{-- Untuk menghindari excaping gunakan echo seperti dibawah ini --}}
