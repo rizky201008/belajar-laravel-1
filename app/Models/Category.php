1d025348193e9734b5a963861cf59fa045bcb0f3
Rizky Agung Prayogi <rizkyden06@gmail.com>
2022-06-25 22:22:52 +0700

    update

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Scout\Searchable;
 
 class Category extends Model
 {
     use HasFactory;
+    use Searchable;
     protected $fillable = ['id', 'name', 'slug'];
     public function post()
     {
