635f0b6f4e4ed77071270bc76f98de9e9d0ef23e
Rizky Agung Prayogi <rizkyden06@gmail.com>
2022-07-02 19:40:52 +0700

    disable angolia and scout feature

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,12 +5,10 @@ namespace App\Models;
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Laravel\Scout\Searchable;
 
 class Category extends Model
 {
     use HasFactory;
-    use Searchable;
     protected $fillable = ['id', 'name', 'slug'];
     public function post()
     {
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,12 +5,10 @@ namespace App\Models;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Laravel\Scout\Searchable;
 
 class Post extends Model
 {
     use HasFactory;
-    use Searchable;
     /*
     *$fillable memperbolehkan pengisian kolom yang ada pada list
     *$guarded tidak memperbolehkan pengisian kolom yang ada pada list
