5ee41cc99b9a9a8af6c0c0a547213da3755adb76
Rizky Agung Prayogi <rizkyden06@gmail.com>
2022-07-12 17:53:10 +0700

    Edit AdminController now admin can edit and delete post

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -34,4 +34,33 @@ class AdminController extends Controller
             'post'=>$post
         ]);
     }
+    public function edit($id){
+        $category = Category::all();
+        $post=Post::find($id);
+        return view('admin.edit',[
+            'title'=>'Edit Post',
+            'post'=>$post,
+            'category'=>$category
+        ]);
+    }
+    public function update(Request $request, $id){
+        $validated = $request->validate([
+            'title' => 'required',
+            'ecpt' => 'required',
+            'catid' => 'required',
+            'body' => 'required'
+        ]);
+        $post = Post::find($id);
+        $post->title = $validated['title'];
+        $post->excerpt = $validated['ecpt'];
+        $post->category_id = $validated['catid'];
+        $post->body = $validated['body'];
+        $post->update();
+        return redirect('/admin/posts');
+    }
+    public function delete($id){
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/admin/posts');
+    }
 }
