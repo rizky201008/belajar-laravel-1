e1d1671d75a52b4f122282afc22f0609c7f1d27d
Rizky Agung Prayogi <rizkyden06@gmail.com>
2022-07-23 11:28:44 +0700

    Update navbar

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -9,7 +9,7 @@
       <ul class="navbar-nav ms-auto">
         @auth
         <li class="nav-item">
-          <a class="nav-link {{ ($title === "Home") ? 'active' : '' }}" href="/admin">Buat Post</a>
+          <a class="nav-link {{ ($title === "Tulis Artikel") ? 'active' : '' }}" href="/admin">Buat Post</a>
         </li>
         <li class="nav-item">
           <a class="nav-link {{ ($title === "Blog") ? 'active' : '' }}" href="/admin/posts">Post</a>
